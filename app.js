c962479b97cfe5955471fdb85392bb0cc4d2ce43
Renukadeveloper96 <renukamaradagi@gmail.com>
2021-11-29 21:49:56 +0530

    first

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ app.use (bodyParser.urlencoded({extended:true}));
 app.use (bodyParser.json());
 app.use (cors());
 //const mongourl ="mongodb://localhost:27017"
-const mongourl ="mongodb+srv://zomato:zomato1277@cluster0.nbutl.mongodb.net/zomato?retryWrites=true&w=majority"
+const mongourl ="mongodb+srv://zomato:zomato1277@cluster0.nbutl.mongodb.net/zomato?retryWrites=true&w=majority";
 var db;  
 
 //get
 app.get('/',(req,res) =>{
-    res.send("welcome to Api 2")
+    res.send("welcome to zomatoapp")
 })
 //list all cities
 app.get('/location',(req,res)=>{
@@ -51,7 +51,7 @@ app.get('/restaurant',(req,res) =>{
     })
 })
 //filterapi
-//(http://localhost:8210/filter/1?lcost=500&hcost=600)
+//(http://localhost:8210/filter/1?lcost=100&hcost=300)
 app.get('/filter/:mealType',(req,res)=>{
     console.log(req.params)
     console.log(req.query)
